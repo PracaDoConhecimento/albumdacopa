97bcb306afae4d3de37a9b3c13c9117b76025911
Felipe Fernandes <ripcorp@gmail.com>
2014-06-09 14:13:45 -0300

    Função de correção de perguntas

--- inc/atualizarpergunta.php
+++ inc/atualizarpergunta.php
@@ -1,12 +1,15 @@
 <?php 
 	require_once('connection.php');
+	require_once('pergunta.php');
+
+	$conn = new Conexao();
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Formulário para atualizar perguntada</title>
-		<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	    <link rel="stylesheet" href="../css/bootstrap.min.css">        
 	    <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
 	    
@@ -39,17 +42,21 @@
 		//Entrar quando o submite da atualização da pergunta acontecer
 		if (!empty($_POST['perguntabusca'])){
 
-			$conn = new Conexao();
 			$conn->Connect();
 			
 			$pergunta=$_POST['perguntabusca'];
-			
-			$result=mysql_query("SELECT * FROM perguntas_respostas WHERE pergunta like '%$pergunta%' ");
+			$sql = "SELECT * FROM perguntas_respostas WHERE pergunta like '%$pergunta%' ";
+			$result=mysql_query($sql) or die(mysql_error());
 			$row = mysql_fetch_array($result);
 			$row_respostas = preg_replace('!s:(\d+):"(.*?)";!e', "'s:'.strlen('$2').':\"$2\";'",$row[2]);
 			
 			
 			$respostas = unserialize($row_respostas);
+			var_dump($respostas);
+
+
+			//$respostas = array_map('utf8_encode', $respostas ); // encode the array again
+
 			$conn->disconnect();
 		
 
@@ -60,14 +67,20 @@
 	            <div class="col-xs-12">
 
 	 				<form action="./atualizarpergunta.php" method="POST" role="form" class="form"> 
-
+	 					<?php 
+							$resposta1 = utf8_decode($respostas[0]);
+							$resposta2 = utf8_decode($respostas[1]);
+							$resposta3 = utf8_decode($respostas[2]);
+							$resposta4 = utf8_decode($respostas[3]);
+	 					?>
+	 					<input type="hidden" name="pergunta" value="<?php print($row[0])?>" >
 	                    <input type="hidden" name="id" value="<?php print($row[0])?>" >
 	                    <h1>Atualize a pergunta:</h1>
-	                    <input type="text" name="perguntabusca" value="<?php print($row[1])?>" class="form-control" ><br/>
-	                    <label>1º Resposta</label> <input type="text"  name="resposta1" class="form-control" value="<?php print($respostas[0]) ?>" > <br/>
-	                    <label>2º Resposta</label> <input type="text" name="resposta2" class="form-control" value="<?php print($respostas[1]) ?>" ><br/>
-	                    <label>3º Resposta</label> <input type="text"  name="resposta3" class="form-control" value="<?php print($respostas[2]) ?>" ><br/>
-	                    <label>4º Resposta</label> <input type="text"  name="resposta4" class="form-control" value="<?php print($respostas[3]) ?>" ><br/>
+	                    <input type="text" name="perguntabusca" value="<?php echo htmlspecialchars($row[1]); ?>" class="form-control" ><br/>
+	                    <label>1º Resposta</label> <input type="text"  name="resposta1" class="form-control" value="<?php echo $resposta1; ?>" > <br/>
+	                    <label>2º Resposta</label> <input type="text"  name="resposta2" class="form-control" value="<?php echo $resposta2; ?>" ><br/>
+	                    <label>3º Resposta</label> <input type="text"  name="resposta3" class="form-control" value="<?php echo $resposta3; ?>" ><br/>
+	                    <label>4º Resposta</label> <input type="text"  name="resposta4" class="form-control" value="<?php echo $resposta4; ?>" ><br/>
 	                    <label>Reposta Certa </label>
 	                   	<input type="text"  name="correta" class="form-control" value=<?php print($row['resposta_certa'])?>><br/>
 	                    <br/>
@@ -86,17 +99,16 @@
 		//Entrar caso o formulário pergunta esteja preenchido
 		if (!empty($_POST['pergunta'])){
 
-			$conn = new Conexao();
+			$perguntaClass = new Pergunta();
+			
+			$id = $_POST['id'];
+			$pergunta_nova = $_POST['pergunta'] ;
+			$respostas_novas = array($_POST['resposta1'],$_POST['resposta2'],$_POST['resposta3'],$_POST['resposta4']);
+			$respostas_novas = utf8_encode($respostas_novas);
+			$respostacorreta_nova = $_POST['correta']; 
 
-			$conn->Connect();
-			$id=$_POST['id'];
-			$pergunta = $_POST['pergunta'] ;
-			$resposta = serialize(array($_POST['resposta1'],$_POST['resposta2'],$_POST['resposta3'],$_POST['resposta4']));
-			$correta = $_POST['correta']; 
-
-			$result=mysql_query("UPDATE perguntas_respostas SET pergunta='$pergunta',respostas='$resposta', resposta_certa='$correta' WHERE id='$id';");
-				
-			$conn->disconnect();
+			$perguntaClass->atualizarPergunta($id, $pergunta_nova, $respostas_novas, $respostacorreta_nova);
+							
 		}
 
 	?>
